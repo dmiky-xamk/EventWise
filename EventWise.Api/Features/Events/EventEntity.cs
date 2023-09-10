6420fdf331adb199396557edca9b1dc60b14d468
dmiky-xamk <dmiky001@edu.xamk.fi>
2023-09-10 14:25:06 +0300

    Add EventParticipant class

--- EventWise.Api/Features/Events/EventEntity.cs
+++ EventWise.Api/Features/Events/EventEntity.cs
@@ -11,3 +11,12 @@ public sealed class EventEntity
     public required DateTime EndDate { get; set; }
     public List<EventParticipant> Participants { get; set; } = new();
 }
+
+public sealed class EventParticipant
+{
+    public required int AppUserId { get; set; }
+    public required AppUser AppUser { get; set; }
+    public required int EventId { get; set; }
+    public required EventEntity Event { get; set; }
+    public required bool IsHost { get; set; }
+}
