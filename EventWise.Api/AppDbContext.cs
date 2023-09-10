81759edf8bddc97a9d03dec71c6743438455b5db
dmiky-xamk <dmiky001@edu.xamk.fi>
2023-09-10 14:47:05 +0300

    Add Identity for authentication purposes

--- EventWise.Api/AppDbContext.cs
+++ EventWise.Api/AppDbContext.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using EventWise.Api.Features.Events;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace EventWise.Api;
 
-public sealed class AppDbContext : DbContext
+public sealed class AppDbContext : IdentityDbContext<AppUser>
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     { }
