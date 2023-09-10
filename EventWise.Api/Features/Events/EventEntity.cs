ae013e1b0bb85380ca28e6c0ac41a00ae923eec3
dmiky-xamk <dmiky001@edu.xamk.fi>
2023-09-10 14:22:31 +0300

    Add EventEntity.cs

---
+++ EventWise.Api/Features/Events/EventEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace EventWise.Api.Features.Events;
+
+public sealed class EventEntity
+{
+    public required int EventId { get; set; }
+    public required string Name { get; set; }
+    public required string Description { get; set; }
+    public required string Address { get; set; }
+    public required string City { get; set; }
+    public required DateTime StartDate { get; set; }
+    public required DateTime EndDate { get; set; }
+    public List<EventParticipant> Participants { get; set; } = new();
+}
