2d21b4f5ecc02a5763b0bedb5f812d640ab02c2a
dmiky-xamk <dmiky001@edu.xamk.fi>
2023-09-12 11:08:17 +0300

    Add events to DbContext

--- EventWise.Api/AppDbContext.cs
+++ EventWise.Api/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using EventWise.Api.Features.Events.Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace EventWise.Api;
@@ -7,4 +8,6 @@ public sealed class AppDbContext : IdentityDbContext<AppUser>
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     { }
+    
+    public required DbSet<EventEntity> Events { get; set; }
 }
