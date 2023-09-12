42243a41f29f0a38e95cf6d115c8bd04b96ed03c
dmiky-xamk <dmiky001@edu.xamk.fi>
2023-09-12 11:04:36 +0300

    Create entities

---
+++ EventWise.Api/Features/Events/Domain/EventParticipantEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace EventWise.Api.Features.Events.Domain;
+
+public sealed class EventParticipantEntity
+{
+    public required string AppUserId { get; set; }
+    public required AppUser AppUser { get; set; }
+    public required int EventId { get; set; }
+    public required EventEntity Event { get; set; }
+    public required bool IsHost { get; set; }
+}
