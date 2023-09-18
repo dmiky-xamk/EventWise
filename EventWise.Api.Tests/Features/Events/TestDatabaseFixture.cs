791ad4cf05256858cf321a39603c1358c1106f97
dmiky-xamk <dmiky001@edu.xamk.fi>
2023-09-18 11:50:16 +0300

    Implement event updating tests

---
+++ EventWise.Api.Tests/Features/Events/TestDatabaseFixture.cs
@@ -0,0 +1,42 @@
+﻿using EventWise.Api.Persistence;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EventWise.Api.Tests.Features.Events;
+
+public class TestDatabaseFixture
+{
+    private const string ConnectionString = "Data Source=EventWise.db";
+
+    private static readonly object Lock = new();
+    private static bool _databaseInitialized;
+
+    public TestDatabaseFixture()
+    {
+        lock (Lock)
+        {
+            if (_databaseInitialized) return;
+
+            using var context = CreateContext();
+
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+
+            _databaseInitialized = true;
+        }
+    }
+    
+    public AppDbContext CreateContext()
+        => new(new DbContextOptionsBuilder<AppDbContext>()
+            .UseSqlite(ConnectionString)
+            .Options);
+
+    public AppDbContext CreateContextWithTransaction()
+    {
+        var context = CreateContext();
+        context.Database.BeginTransaction();
+
+        return context;
+    }
+}
---
+++ EventWise.Api.Tests/Features/Events/UpdateEventTests.cs
@@ -0,0 +1,126 @@
+﻿using AutoFixture;
+using EventWise.Api.Features.Events.UpdateEvent;
+using EventWise.Api.Persistence;
+using FluentAssertions;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using NSubstitute;
+using OneOf.Types;
+
+namespace EventWise.Api.Tests.Features.Events;
+
+public sealed class UpdateEventTests : IClassFixture<TestDatabaseFixture>
+{
+    private readonly UserManager<AppUser> _mockUserManager;
+    private readonly IHttpContextAccessor _mockHttpContextAccessor;
+    private readonly Fixture _fixture = new();
+    private TestDatabaseFixture TestDbFixture { get; }
+    private readonly UpdateEventRequestValidator _validator = new();
+
+    public UpdateEventTests(TestDatabaseFixture testDbFixture)
+    {
+        TestDbFixture = testDbFixture;
+        _mockHttpContextAccessor = Substitute.For<IHttpContextAccessor>();
+        _mockUserManager = TestHelpers.MockUserManager();
+        _fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+    }
+
+    [Fact]
+    public async Task UpdateEvent_UpdatesAndReturnsEvent_WhenSuccessful()
+    {
+        // Arrange
+        await using var context = TestDbFixture.CreateContextWithTransaction();
+        var user = CreateUser();
+        var eventEntity = TestHelpers.CreateEventAsHost(_fixture, context, user);
+        var expected = _fixture.Build<UpdateEventRequest>()
+            .With(x => x.StartDate, DateTime.Now.AddDays(1))
+            .With(x => x.EndDate, DateTime.Now.AddDays(2))
+            .Create();
+
+        var sut = new UpdateEvent.Handler(context, _mockHttpContextAccessor, _mockUserManager, _validator);
+
+        // Act
+        var actual = await sut.Handle(new UpdateEvent.Command { Event = expected, PublicId = eventEntity.PublicId },
+            default);
+        context.ChangeTracker.Clear();
+
+        // Assert
+        actual.Value.As<UpdateEventResponse>().Event.Should().BeEquivalentTo(expected);
+        actual.Value.As<UpdateEventResponse>().Event.Participants.Should()
+            .ContainSingle(x => x.Username == user.UserName);
+    }
+
+    [Fact]
+    public async Task UpdateEvent_ReturnsNotFound_WhenEventDoesNotExist()
+    {
+        // Arrange
+        await using var context = TestDbFixture.CreateContext();
+        CreateUser();
+        var expected = _fixture.Build<UpdateEventRequest>()
+            .With(x => x.StartDate, DateTime.Now.AddDays(1))
+            .With(x => x.EndDate, DateTime.Now.AddDays(2))
+            .Create();
+
+        var sut = new UpdateEvent.Handler(context, _mockHttpContextAccessor, _mockUserManager, _validator);
+
+        // Act
+        var actual =
+            await sut.Handle(new UpdateEvent.Command { Event = expected, PublicId = _fixture.Create<string>() },
+                default);
+
+        // Assert
+        actual.Value.Should().BeOfType<NotFound>();
+    }
+
+    [Fact]
+    public async Task UpdateEvent_ReturnsNotFound_WhenUserIsNotHost()
+    {
+        // Arrange
+        await using var context = TestDbFixture.CreateContext();
+        var user = CreateUser();
+        var eventEntity = TestHelpers.CreateEventAsParticipant(_fixture, context, user);
+        var request = _fixture.Build<UpdateEventRequest>()
+            .With(x => x.StartDate, DateTime.Now.AddDays(1))
+            .With(x => x.EndDate, DateTime.Now.AddDays(2))
+            .Create();
+
+        var sut = new UpdateEvent.Handler(context, _mockHttpContextAccessor, _mockUserManager, _validator);
+
+        // Act
+        var actual = await sut.Handle(new UpdateEvent.Command { Event = request, PublicId = eventEntity.PublicId },
+            default);
+
+        // Assert
+        actual.Value.Should().BeOfType<NotFound>();
+    }
+
+    [Fact]
+    public async Task UpdateEvent_ReturnsError_WhenValidationFails()
+    {
+        // Arrange
+        await using var context = TestDbFixture.CreateContext();
+        CreateUser();
+        var request = _fixture.Build<UpdateEventRequest>()
+            .With(x => x.Name, string.Empty)
+            .Create();
+
+        var sut = new UpdateEvent.Handler(context, _mockHttpContextAccessor, _mockUserManager, _validator);
+
+        // Act
+        var actual = await sut.Handle(new UpdateEvent.Command { Event = request, PublicId = _fixture.Create<string>() },
+            default);
+
+        // Assert
+        actual.Value.Should().BeOfType<ValidationResult>();
+    }
+    
+    private AppUser CreateUser()
+    {
+        var user = _fixture.Create<AppUser>();
+        _mockUserManager.FindByIdAsync(Arg.Any<string>()).Returns(user);
+
+        return user;
+    }
+
+}
