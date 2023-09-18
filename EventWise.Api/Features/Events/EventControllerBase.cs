133513ae1939181107c7fc87e761e5e019b3fe59
dmiky-xamk <dmiky001@edu.xamk.fi>
2023-09-18 11:48:36 +0300

    Extract returning validation errors to a base controller

---
+++ EventWise.Api/Features/Events/EventControllerBase.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace EventWise.Api.Features.Events;
+
+public class EventControllerBase : ControllerBase
+{
+    protected ModelStateDictionary CreateModelStateErrorsFrom(ValidationResult validationResult)
+    {
+        foreach (var error in validationResult.Errors)
+        {
+            ModelState.AddModelError(error.PropertyName, error.ErrorMessage);
+        }
+
+        return ModelState;
+    }
+}
