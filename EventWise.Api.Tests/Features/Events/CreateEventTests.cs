f84ffe6e14ebd444b6b066408d1b9f8177b9c026
dmiky-xamk <dmiky001@edu.xamk.fi>
2023-09-18 11:51:28 +0300

    Refactor

--- EventWise.Api.Tests/Features/Events/CreateEventTests.cs
+++ EventWise.Api.Tests/Features/Events/CreateEventTests.cs
@@ -4,7 +4,6 @@ using EventWise.Api.Persistence;
 using FluentAssertions;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using NSubstitute;
@@ -23,16 +22,7 @@ public sealed class CreateEventTests : IClassFixture<TestDatabaseFixture>
     {
         TestDbFixture = testDbFixture;
         _mockHttpContextAccessor = Substitute.For<IHttpContextAccessor>();
-        _mockUserManager = Substitute.For<UserManager<AppUser>>(
-            Substitute.For<IUserStore<AppUser>>(),
-            Substitute.For<IOptions<IdentityOptions>>(),
-            Substitute.For<IPasswordHasher<AppUser>>(), 
-            Array.Empty<IUserValidator<AppUser>>(),
-            Array.Empty<IPasswordValidator<AppUser>>(),
-            Substitute.For<ILookupNormalizer>(),
-            Substitute.For<IdentityErrorDescriber>(),
-            Substitute.For<IServiceProvider>(),
-            Substitute.For<ILogger<UserManager<AppUser>>>());
+        _mockUserManager = TestHelpers.MockUserManager();
         _fixture.Behaviors.Add(new OmitOnRecursionBehavior());
     }
 
@@ -59,41 +49,5 @@ public sealed class CreateEventTests : IClassFixture<TestDatabaseFixture>
         actual.Value.As<CreateEventResponse>().Event.Participants.Should()
             .ContainSingle(x => x.Username == user.UserName);
         actual.Value.As<CreateEventResponse>().Event.PublicId.Should().NotBeNull();
-    }
-}
-
-public class TestDatabaseFixture
-{
-    private const string ConnectionString = "Data Source=EventWise.db";
-
-    private static readonly object Lock = new();
-    private static bool _databaseInitialized;
-
-    public TestDatabaseFixture()
-    {
-        lock (Lock)
-        {
-            if (_databaseInitialized) return;
-
-            using var context = CreateContext();
-
-            context.Database.EnsureDeleted();
-            context.Database.EnsureCreated();
-
-            _databaseInitialized = true;
-        }
-    }
-
-    private AppDbContext CreateContext()
-        => new(new DbContextOptionsBuilder<AppDbContext>()
-            .UseSqlite(ConnectionString)
-            .Options);
-
-    public AppDbContext CreateContextWithTransaction()
-    {
-        var context = CreateContext();
-        context.Database.BeginTransaction();
-
-        return context;
     }
 }
--- EventWise.Api/Features/Events/CreateEvent/CreateEventRequest.cs
+++ EventWise.Api/Features/Events/CreateEvent/CreateEventRequest.cs
@@ -9,39 +9,11 @@ public class CreateEventRequestValidator : AbstractValidator<CreateEventRequest>
 {
     public CreateEventRequestValidator()
     {
-        RuleFor(x => x.Name)
-            .NotEmpty()
-            .MaximumLength(50)
-            .WithMessage("Event name is required and must not exceed 50 characters");
-
-        RuleFor(x => x.Description)
-            .NotEmpty()
-            .MaximumLength(100)
-            .WithMessage("Event description is required and must not exceed 100 characters");
-
-        RuleFor(x => x.Address)
-            .NotEmpty()
-            .MaximumLength(100)
-            .WithMessage("Event address is required and must not exceed 100 characters");
-
-        RuleFor(x => x.City)
-            .NotEmpty()
-            .MaximumLength(50)
-            .WithMessage("City name is required and must not exceed 50 characters");
-        
-        RuleFor(x => x.StartDate)
-            .NotEmpty()
-            .Must(BeAValidFutureDate).WithMessage("Start date must be a valid future date")
-            .LessThan(x => x.EndDate).WithMessage("Start date must be before end date");
-
-        RuleFor(x => x.EndDate)
-            .NotEmpty()
-            .Must(BeAValidFutureDate).WithMessage("End date must be a valid future date")
-            .GreaterThan(x => x.StartDate).WithMessage("End date must be after start date");
-    }
-    
-    private static bool BeAValidFutureDate(DateTime date)
-    {
-        return date >= DateTime.UtcNow;
+        RuleFor(x => x.Name).EventName();
+        RuleFor(x => x.Description).EventDescription();
+        RuleFor(x => x.Address).EventAddress();
+        RuleFor(x => x.City).EventCity();
+        RuleFor(x => x.StartDate).EventStartDate(x => x.EndDate);
+        RuleFor(x => x.EndDate).EventEndDate(x => x.StartDate);
     }
 }
